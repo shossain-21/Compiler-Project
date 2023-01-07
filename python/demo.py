8ad5717957877123ccd87afe9f114d4bab62226c
Kanita-Haider <kanitahaider44@gmail.com>
2023-01-07 19:19:12 +0600

    python compiler

---
+++ python/demo.py
@@ -0,0 +1,8 @@
+def my_function(x):
+  return 27/x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
+
+
---
+++ python/sample_input2.py
@@ -0,0 +1,6 @@
+def add (a,b):
+             return a + b
+
+print(add(56,23))
+
+
